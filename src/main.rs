ad588df834dd29c30414220c48cc63e5b598a7de
Lucas <lucasclaudinocc@gmail.com>
2024-01-10 15:30:45 -0300

    Added file name query

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,25 @@
 use std::io;
-// Import file
 use std::fs::File;
+use std::io::Write; // Import Write trait
 
-fn script_creator (line: &str) {
-    let mut script = String::new();
+fn script_creator(line: &str, file_name: &str) {
+    let mut script = String::from("#!/bin/bash\n");
     script.push_str(line);
-    // Turn script into a .sh file
-    script.push_str("#!/bin/bash\n");
-    // Create file
-    file_creator(&script);
+    file_creator(&script, file_name);
 }
 
-fn file_creator (content: &str) {
-    let mut file = String::new();
-    file.push_str(content);
-    // Create file
-    let mut file = File::create("script.sh").unwrap();
+fn file_creator(content: &str, file_name: &str)) {
+    let mut file = File::create(file_name + ".sh").expect("Unable to create file");
+    file.write_all(content.as_bytes()).expect("Unable to write to file");
 }
 
 fn main() {
-    let mut line = String::new();
     println!("Digite uma linha para adicionar ao script: ");
-    std::io::stdin().read_line(&mut line).unwrap();
-    script_creator(&line);
+    let mut line = String::new();
+    let mut file_name = String::new();
+    io::stdin().read_line(&mut line).expect("Falha ao ler linha");
+    println!("Nome do arquivo: (não incluir o .sh)");
+    io::stdin().read_line(&mut file_name).expect("Falha ao ler nome do arquivo");
+    script_creator(&line, &file_name);
     println!("Script criado com sucesso!");
 }
